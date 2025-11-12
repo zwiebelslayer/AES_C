deb8dc22a38a2a23795681c6ffa56c5a0811a1c0
zwiebelslayer <pasi2603@hotmail.de>
2025-11-12 21:02:39 +0100

    init commit

---
+++ aes.h
@@ -0,0 +1,9 @@
+
+#ifndef AES_C_AES_H
+#define AES_C_AES_H
+
+#define BLOCK_SIZE 16 // 128 bit @ 8bit per cell ==> 16 cells => Block, often printed as a table in books
+
+
+
+#endif //AES_C_AES_H
---
+++ readme.md
@@ -0,0 +1,5 @@
+# AES implementation in C
+
+Yeah never use this btw :)
+
+- just implements the encryption, decryption is left as an exercise for the reader :)
